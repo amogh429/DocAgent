a1e672c0c8b68115e40ad1b21d6726e326b23a52
amogh429 <amogh.allrounder@gmail.com>
2026-08-18 11:23:33 +0530

    feat: Build Agent Route

---
+++ Backend/src/routes/agent.routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import crypto from "crypto";
+import { runAgentLoop } from "../agent/loop.js";
+
+const router = express.Router();
+
+router.post("/ask", async(req,res) => {
+    const taskId = crypto.randomUUID();
+    const question = req.body.original_question;
+
+    try {
+        const result = await runAgentLoop(question , taskId);
+        res.json(result);
+    } catch(error){
+        console.error(error);
+        res.status(500).json({ error: "Something went wrong" })
+    }
+})
+
+export default router;
+
