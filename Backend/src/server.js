492e51eeb8fff77e2cc70613b23fd3a0c08a27e0
amogh429 <amogh.allrounder@gmail.com>
2026-09-03 11:24:11 +0530

    feat: Implement CORS

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import express from "express";
 import connectDB from "./config/db.js";
 import agentRoutes from "./routes/agent.routes.js";
 
 await connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/agent", agentRoutes);
