3ad37372a71bb935c5e8f25d6485aba931cca556
amogh429 <amogh.allrounder@gmail.com>
2026-09-05 12:21:46 +0530

    feat: Edit redis.js

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,6 +1,13 @@
-const connection = {
-  host: "127.0.0.1",
-  port: 6379
-};
+const connection = process.env.REDIS_URL
+  ? (() => {
+      const { hostname, port, password } = new URL(process.env.REDIS_URL);
+      return {
+        host: hostname,
+        port: Number(port),
+        password,
+        tls: {}
+      };
+    })()
+  : { host: "127.0.0.1", port: 6379 };
 
 export default connection;
